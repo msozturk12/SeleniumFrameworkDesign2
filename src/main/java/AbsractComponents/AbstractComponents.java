4685fbc6684c0f6ddb3380b19c2546924c91f5c5
mesut <msozturk12@gmail.com>
2025-08-15 23:41:03 +0200

    neu Code für GitHub

--- src/main/java/AbsractComponents/AbstractComponents.java
+++ src/main/java/AbsractComponents/AbstractComponents.java
@@ -58,6 +58,7 @@ public class AbstractComponents {
         Thread.sleep(3000);
         //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         //wait.until(ExpectedConditions.invisibilityOf(ele));
+        System.out.println("Test für Github übung");
 
     }
 
